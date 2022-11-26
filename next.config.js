975a7478562ce64483da612f65c997b6cc196551
roy <roy>
2022-11-26 14:17:21 +0800

    modify next config,modify next yml

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-const { i18n } = require("./next-i18next.config");
-
 module.exports = {
-  i18n,
   pageExtensions: ["page.js", "page.jsx"],
+  images: {
+    loader: "akamai",
+    path: "",
+  },
 };
